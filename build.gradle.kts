9b4947bd4b2f20792d16ff883157448fb49adfd8
Tom Seifert <14896745+Syex@users.noreply.github.com>
2024-01-23 10:02:23 +0100

    Add more KMP targets

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,19 +40,18 @@ kotlin {
         }
     }
 
+    macosX64()
+    macosArm64()
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    tvosArm64()
+    tvosSimulatorArm64()
+    tvosX64()
+    
     mingwX64()
     linuxX64()
     linuxArm64()
-
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
-
 
     sourceSets {
         val commonMain by getting
@@ -67,7 +66,5 @@ kotlin {
         val jvmTest by getting
         val jsMain by getting
         val jsTest by getting
-        val nativeMain by getting
-        val nativeTest by getting
     }
 }
